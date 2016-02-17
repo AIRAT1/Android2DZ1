c51ec0b9931148458feaf2b21813888b49f65e33
Ayrat <ayrat1@mail.ru>
2016-02-17 21:51:11 +0300

    Create DBHelper class

---
+++ app/src/main/java/data/DBHelper.java
@@ -0,0 +1,37 @@
+package data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+    private static final String TABLE_MYTABLE = "myTable";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_COMPANY_NAME = "companyName";
+    public static final String DATABASE_MYTABLE = "CREATE TABLE "
+            + TABLE_MYTABLE + " (" + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_COMPANY_NAME + " text);";
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.beginTransaction();
+        try {
+            db.execSQL(DATABASE_MYTABLE);
+            db.setTransactionSuccessful();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MYTABLE);
+        onCreate(db);
+    }
+}
